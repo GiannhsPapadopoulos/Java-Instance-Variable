7a25d6a32ea079416f8d3f68ec60a1bf2ab203a0
JP <papadopoulos.giannhs.soft@gmail.com>
2026-03-04 18:05:20 +0200

    Add files via upload

---
+++ InstanceVariable/src/Dog.java
@@ -0,0 +1,15 @@
+
+public class Dog {
+
+    private String name; // Instance Variable
+
+    // Constructor
+    public Dog(String name) {
+        this.name = name;
+    }
+
+    // Instance Method
+    public void printName() {
+        System.out.println("name = " + this.name);
+    }
+}
